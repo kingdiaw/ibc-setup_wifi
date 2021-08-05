9b00f83a7836bae75dd737f96072e9843ab2f0fa
kingdiaw <kingdiawehsut@gmail.com>
2021-08-05 08:57:16 +0800

    Add files via upload
    setup wifi

---
+++ setup_wifi.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+
+//Replace the next variables with your SSID/Password combination
+const char* ssid = "";
+const char* password = "";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  setup_wifi();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address:");
+  Serial.println(WiFi.localIP());
+}
